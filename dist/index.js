68e858b85ae303023e926763264360cf1a5bdd3f
jayson <leobest9688@gmail.com>
2022-07-07 00:14:29 +0800

    tuple

---
+++ dist/index.js
@@ -0,0 +1,10 @@
+"use strict";
+let id = 5;
+let company = 'Goldwiz';
+let isPublished = true;
+let x = 'Hello';
+let ids = [1, 2, 3, 4, 5];
+let arr = [1, true, 'Hello'];
+// ids.push('Hello')
+//Tuple
+let person = [1, 'Jayson', true];
---
+++ src/index.ts
@@ -0,0 +1,12 @@
+let id: number = 5
+let company: string = 'Goldwiz'
+let isPublished: boolean = true
+let x: any = 'Hello'
+
+let ids: number[] = [1,2,3,4,5]
+let arr: any[]= [1, true, 'Hello']
+// ids.push('Hello')
+
+//Tuple
+let person: [number, string, boolean] = [1, 'Jayson', true]
+
