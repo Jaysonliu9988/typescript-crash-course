a6212f27dd62ec801b9a1b82207f6c49726f3a32
jayson <leobest9688@gmail.com>
2022-07-07 23:32:41 +0800

    interfaces

--- dist/index.js
+++ dist/index.js
@@ -8,3 +8,43 @@ let arr = [1, true, 'Hello'];
 // ids.push('Hello')
 //Tuple
 let person = [1, 'Jayson', true];
+// Tuple Array
+let employee;
+employee = [
+    [1, 'Brad'],
+    [2, 'John'],
+    [3, 'Jill'],
+];
+// Union
+let pid;
+pid = '22';
+// Enum
+var Direction1;
+(function (Direction1) {
+    Direction1[Direction1["Up"] = 1] = "Up";
+    Direction1[Direction1["Down"] = 2] = "Down";
+    Direction1[Direction1["Left"] = 3] = "Left";
+    Direction1[Direction1["Right"] = 4] = "Right";
+})(Direction1 || (Direction1 = {}));
+const user = {
+    id: 1,
+    name: 'John',
+};
+// Type Assertion
+let cid = 1;
+// let customerId = <number>cid
+let customerId = cid;
+// Functions
+function addNum(x, y) {
+    return x + y;
+}
+// Void
+function log(message) {
+    console.log(message);
+}
+const user1 = {
+    id: 1,
+    name: 'John',
+};
+const add = (x, y) => x + y;
+const sub = (x, y) => x - y;
--- src/index.ts
+++ src/index.ts
@@ -3,10 +3,78 @@ let company: string = 'Goldwiz'
 let isPublished: boolean = true
 let x: any = 'Hello'
 
-let ids: number[] = [1,2,3,4,5]
-let arr: any[]= [1, true, 'Hello']
+let ids: number[] = [1, 2, 3, 4, 5]
+let arr: any[] = [1, true, 'Hello']
 // ids.push('Hello')
 
 //Tuple
 let person: [number, string, boolean] = [1, 'Jayson', true]
+
+// Tuple Array
+
+let employee: [number, string][]
+
+employee = [
+    [1, 'Brad'],
+    [2, 'John'],
+    [3, 'Jill'],
+]
+
+// Union
+let pid: string | number
+pid = '22'
+
+// Enum
+enum Direction1 {
+    Up = 1,
+    Down,
+    Left,
+    Right,
+}
+
+
+// Objects
+type User = {
+    id: number
+    name: string
+}
+
+const user: User = {
+    id: 1,
+    name: 'John',
+}
+
+// Type Assertion
+let cid: any = 1
+// let customerId = <number>cid
+let customerId = cid as number
+
+// Functions
+function addNum(x: number, y: number): number {
+    return x + y
+}
+
+// Void
+function log(message: string | number): void {
+    console.log(message)
+}
+
+// Interfaces
+interface UserInterface {
+    readonly id: number
+    name: string
+    age?: number
+}
+
+const user1: UserInterface = {
+    id: 1,
+    name: 'John',
+}
+
+interface MathFunc {
+    (x: number, y: number): number
+}
+
+const add: MathFunc = (x: number, y: number): number => x + y
+const sub: MathFunc = (x: number, y: number): number => x - y
 
